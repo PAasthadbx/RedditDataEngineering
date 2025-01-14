2544d4351b75a7e072b7367fdcd57a4f9b7f6b27
PAasthadbx <poudelaastha1997@gmail.com>
2025-01-14 20:54:14 +0400

    updated reddit dag

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -9,11 +9,4 @@ def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=Non
     instance = connect_reddit(CLIENT_ID, SECRET, 'Agent DBX')
     # extraction
     posts = extract_posts(instance, subreddit, time_filter, limit)
-    post_df = pd.DataFrame(posts)
-    # transformation
-    post_df = transform_data(post_df)
-    # loading to csv
-    file_path = f'{OUTPUT_PATH}/{file_name}.csv'
-    load_data_to_csv(post_df, file_path)
-
-    return file_path
+    
